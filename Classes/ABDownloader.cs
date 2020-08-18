2fadd232547685f22937c0dccb2686d5da23d001
wangchaolun <wangchaolun@gg.com>
2020-08-18 15:00:40 +0800

    - Split AssetBundle Module

---
+++ Classes/ABDownloader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace UniEasy
+{
+    public class ABDownloader
+    {
+        public ulong ContentBytes;
+        public ulong DownloadedBytes;
+        public float DownloadProgress;
+        private string abName;
+        private string abDownLoadPath;
+        private Hash128 abHash;
+
+        public bool IsVersionCached
+        {
+            get
+            {
+                return Caching.IsVersionCached(abDownLoadPath, abHash);
+            }
+        }
+
+        public ABDownloader(string abName, Hash128 abHash)
+        {
+            this.abName = abName;
+            this.abHash = abHash;
+            abDownLoadPath = PathsUtility.GetWWWPath() + "/" + this.abName;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ABExplorer
+# ABExplorer - Unity AssetBundle 管理插件
+
+安装方法
+---
+
+- 做为子模块添加 `git submodule add https://github.com/chaolunner/ABExplorer.git Assets/ABExplorer`
+
